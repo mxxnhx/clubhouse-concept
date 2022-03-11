fbba6c2f3c18bcf95c20ddf172c492de1c6de5a3
mxxnhx <mhc3357@gmail.com>
2022-03-12 00:29:22 +0900

    Change LiveRoomMember to StatefulWidget

--- lib/ui/liveroom/LiveRoomMember.dart
+++ lib/ui/liveroom/LiveRoomMember.dart
@@ -1,15 +1,10 @@
+import 'package:clubhouse/ui/liveroom/LiveRoom.dart';
 import 'package:clubhouse/utils/DynamicColor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LiveRoomMember extends StatelessWidget {
-  final bool isMuted;
-  final bool isNewMember;
-  final bool isModerator;
-  final String name;
-  final String imagePath;
-  final Color? color;
-  const LiveRoomMember({
+class LiveRoomMember extends StatefulWidget {
+  LiveRoomMember({
     Key? key,
     required this.name,
     required this.imagePath,
@@ -19,59 +14,88 @@ class LiveRoomMember extends StatelessWidget {
     this.isModerator = false,
   }) : super(key: key);
 
+  final bool isMuted;
+  final bool isNewMember;
+  final bool isModerator;
+  final String name;
+  final String imagePath;
+  final Color? color;
+
+  @override
+  _LiveRoomMemberState createState() => _LiveRoomMemberState(isMuted);
+}
+
+class _LiveRoomMemberState extends State<LiveRoomMember> {
+  late bool isMuted;
+
+  _LiveRoomMemberState(isMuted) {
+    this.isMuted = isMuted;
+  }
+
+  void toggleMute() {
+    setState(() {
+      isMuted = !isMuted;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Column(
       children: [
         Expanded(
-          child: AspectRatio(
-            aspectRatio: 1 / 1,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                AspectRatio(
-                  aspectRatio: 1 / 1,
-                  child: CircleAvatar(
-                    backgroundColor: color ?? Colors.grey.shade100,
-                    backgroundImage: AssetImage(imagePath),
-                  ),
-                ),
-                if (isNewMember)
-                  Align(
-                    alignment: Alignment.bottomLeft,
+          child: GestureDetector(
+            onTap: () {
+              toggleMute();
+            },
+            child: AspectRatio(
+              aspectRatio: 1 / 1,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 1 / 1,
                     child: CircleAvatar(
-                      radius: 13,
-                      child: Text("🎉"),
-                      backgroundColor: DynamicColor.withBrightness(
-                        context: context,
-                        color: Colors.white,
-                        darkColor: theme.backgroundColor,
-                      ),
+                      backgroundColor: widget.color ?? Colors.grey.shade100,
+                      backgroundImage: AssetImage(widget.imagePath),
                     ),
                   ),
-                if (isMuted)
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: CircleAvatar(
-                      radius: 13,
-                      backgroundColor: DynamicColor.withBrightness(
-                        context: context,
-                        color: Colors.white,
-                        darkColor: theme.backgroundColor,
-                      ),
-                      child: Icon(
-                        CupertinoIcons.mic_slash,
-                        size: 16,
-                        color: DynamicColor.withBrightness(
+                  if (widget.isNewMember)
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: CircleAvatar(
+                        radius: 13,
+                        child: Text("🎉"),
+                        backgroundColor: DynamicColor.withBrightness(
                           context: context,
-                          color: Colors.grey.shade800,
-                          darkColor: Colors.white,
+                          color: Colors.white,
+                          darkColor: theme.backgroundColor,
                         ),
                       ),
                     ),
-                  ),
-              ],
+                  if (isMuted)
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: CircleAvatar(
+                        radius: 13,
+                        backgroundColor: DynamicColor.withBrightness(
+                          context: context,
+                          color: Colors.white,
+                          darkColor: theme.backgroundColor,
+                        ),
+                        child: Icon(
+                          CupertinoIcons.mic_slash,
+                          size: 16,
+                          color: DynamicColor.withBrightness(
+                            context: context,
+                            color: Colors.grey.shade800,
+                            darkColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
             ),
           ),
         ),
@@ -79,7 +103,7 @@ class LiveRoomMember extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (isModerator)
+            if (widget.isModerator)
               Stack(
                 children: [
                   Icon(
@@ -96,7 +120,7 @@ class LiveRoomMember extends StatelessWidget {
               ),
             SizedBox(width: 3),
             Text(
-              "$name",
+              "${widget.name}",
               style: TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.w500,
@@ -108,3 +132,26 @@ class LiveRoomMember extends StatelessWidget {
     );
   }
 }
+
+// class _LiveRoomMemberState extends State<LiveRoomMember> {
+//   final bool isMuted;
+//   final bool isNewMember;
+//   final bool isModerator;
+//   final String name;
+//   final String imagePath;
+//   final Color? color;
+//   const LiveRoomMember({
+//     Key? key,
+//     required this.name,
+//     required this.imagePath,
+//     this.color,
+//     this.isMuted = true,
+//     this.isNewMember = false,
+//     this.isModerator = false,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//   }
+// }
